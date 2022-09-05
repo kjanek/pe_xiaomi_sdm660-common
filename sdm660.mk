26ede7ff75ff9f708c2f40c44092421295b420ff
Ayush <ayushbisht5663@gmail.com>
2022-09-05 09:47:26 +0530

    sdm660-common: patch libwvhidl to load libprotobuf-cpp-lite-v29
    Error
     E RILD    : dlopen failed: dlopen failed: cannot locate symbol _ZN6google8protobuf8internal13empty_string_E referenced by "/vendor/lib64/libwvhidl.so"...
    
    Signed-off-by: Ayush <ayushbisht5663@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -66,6 +66,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0 \
     android.hardware.drm@1.2-service.widevine
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
