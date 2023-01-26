3dfb76e83885a6165e8ec7ea6f065ac0605f329e
kjanek <krzysztof.janeczek.pl@gmail.com>
2023-01-26 23:35:31 +0100

    sdm660-common: build: fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,8 @@ endif
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_LLVM_BINUTILS := false
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
