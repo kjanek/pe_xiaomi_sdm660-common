a8c890153fed2f9ed8643f1a8dbb6c8b2591bd00
kjanek <krzysztof.janeczek.pl@gmail.com>
2023-02-03 17:14:39 +0100

    sdm660-common: delete obsolete stuff from .mk

--- sdm660.mk
+++ sdm660.mk
@@ -59,12 +59,9 @@ PRODUCT_COPY_FILES += \
 
 # A13
 PRODUCT_PACKAGES += \
-    libsecureui \
-    libsecureui.vendor \
     vendor.qti.hardware.display.allocator@3.0.vendor \
     vendor.qti.hardware.display.allocator@4.0.vendor \
-    vendor.qti.hardware.perf@2.0 \
-    android.hardware.drm@1.2-service.widevine
+    vendor.qti.hardware.perf@2.0
 
 # VNDK
 PRODUCT_COPY_FILES += \
@@ -176,8 +173,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
@@ -203,7 +198,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
